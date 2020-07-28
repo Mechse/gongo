9f0fc7aeb0892e873c3ac00ac6380ac4bca16812
Maximilian Leodolter <max.leo2000@gmail.com>
2020-07-28 19:01:33 +0200

    prototype

--- gongo.go
+++ gongo.go
@@ -1,10 +1,25 @@
 package gongo
 
 import (
+	"context"
 	"fmt"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-// HelloWorld to test package
-func HelloWorld() {
-	fmt.Println("Hello World")
+type gongoClient struct {
+	URI    string
+	Client *mongo.Client
+}
+
+func (d *gongoClient) Init() {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(d.URI))
+	if err != nil {
+		fmt.Printf("Error: %v", err)
+	}
+	d.Client = client
 }
